d29c82b86972eaf3d0980054cc223999fbb30320
J.E.Campbell <jec1f12@uos-12733.clients.soton.ac.uk>
2015-12-02 10:21:06 +0000

    added restart option and remote worker shutdown

--- ga_controller.py
+++ ga_controller.py
@@ -2,26 +2,34 @@ import cPickle
 import argparse
 import ga_crossover
 import ga_setup_selection
+import ga_ring_crossover
 import glob
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import celery
 from celery import group
 from tasks import run_gauss, run_gauss2
+from celery.task.control import broadcast
 from celery.task.control import discard_all
 discard_all()
+
 
 parser = argparse.ArgumentParser(description="-"*78 + "Genetic algorithm controller".ljust(78) + "-"*78)
 parser.add_argument("-pop", "--population_size", type=int, help="Size of initial population", default=100)
 parser.add_argument("-n_number", "--nitrogen_number", type=int, help="Number of intial nitrogens placed into molecule", default=5)
-parser.add_argument("-x_type", "--crossover_type", type=str, help="Type of crossover used, either fixed N or varying", default="varying")
+parser.add_argument("-x_type", "--crossover_type", type=str, help="Type of crossover used, either fixed N/varying or ring crossover", default="ring")
 parser.add_argument("-x_rate", "--crossover_rate", type=int, help="amount of each generation produced by crossover (%%)", default=90)
 parser.add_argument("-e_rate", "--elitism_rate", type=int, help="amount of each generation produced by elitism (%%)", default=10)
-#parser.add_argument("-setup", "--setup_ga_run", type=str, help="setup new run or continue current one", default="Y")
 parser.add_argument("-n_gen", "--number_of_gens", type=int, help="how many generations to run for", default=50)
+parser.add_argument("-restart", "--restart_ga", type=int, help="restart ga from this generation",default=None)
+parser.add_argument("-gauss_calc", "--gauss_calc",type=str,help="which molecular property you want to optimise, reorg_en, dipole etc",default="reorg_en")
+
+
 
 args = parser.parse_args()
 number_of_gens = range(1,args.number_of_gens+1)
+if args.restart_ga:
+   number_of_gens = range(args.restart_ga, args.number_of_gens+1)
 print "Beginning ga run with "+str(args.population_size)+" pop members for "+str(args.number_of_gens)+" generations"
 for ng in number_of_gens:
     gen_name = str(args.population_size)+"_"+str(ng)
@@ -33,9 +41,6 @@ for ng in number_of_gens:
         pop = ga_setup_selection.generate_initial_population(smiles_string,args.population_size,args.nitrogen_number)
         #print len(pop)
         mol_lst = ga_setup_selection.mol_name_maker(pop)
-        all_mol_coords = ga_setup_selection.coords_generator(mol_lst)
-        com_files_neut = ga_setup_selection.com_generator_neutral_grndst(all_mol_coords)
-        com_files_neg = ga_setup_selection.com_generator_neg_mol_neg_geom(all_mol_coords)
         #writing generation .p
         dump_list = []
         for m in mol_lst:
@@ -46,6 +51,9 @@ for ng in number_of_gens:
             dump_list.append(pm)
         cPickle.dump(dump_list, open(p_name, "w+"))
         #collecting and running coms
+        all_mol_coords = ga_setup_selection.coords_generator(mol_lst)
+        com_files_neut = ga_setup_selection.com_generator_neutral_grndst(all_mol_coords)
+        com_files_neg = ga_setup_selection.com_generator_neg_mol_neg_geom(all_mol_coords)
         com_files = com_files_neut + com_files_neg
         job = group([run_gauss.s(com) for com in com_files])
         result = job.apply_async()
@@ -109,8 +117,13 @@ for ng in number_of_gens:
         #print len(pairs_set)
         children = []
         for p in pairs_set:
-            kids = ga_crossover.crossover_pair_varying(p[0],p[1])
-            children+=kids
+            while True:
+                try:
+                    kids = ga_ring_crossover.ring_crossover(p[0],p[1])
+                    children+=kids
+                    break
+                except (IndexError,RuntimeError,ValueError):
+                    continue  
         #print len(children)
         #combining elite and crossed over mols
         mol_list_2 = []
@@ -143,9 +156,10 @@ for ng in number_of_gens:
         print len(run_mols)
         if len(run_mols) <= 1:
             print "GA has hit generation of size 1, will quit now"
+            print "Shutting down workers"
+            broadcast("shutdown")
             exit()
         all_mol_coords = ga_setup_selection.coords_generator(run_mols)
-        print len(all_mol_coords)
         com_files_neut = ga_setup_selection.com_generator_neutral_grndst(all_mol_coords)
         com_files_neg = ga_setup_selection.com_generator_neg_mol_neg_geom(all_mol_coords)
         com_files = com_files_neut + com_files_neg 
@@ -195,8 +209,8 @@ for ng in number_of_gens:
             dump_list.append(pm)
         cPickle.dump(dump_list, open(gen_name+"_results.p", "w+"))
         #setting crossover/elitism sizes
-        #crossover_size = int(args.population_size) * args.crossover_rate/100
-        #elite_size = int(args.population_size) * args.elitism_rate/100
+        crossover_size = int(args.population_size) * args.crossover_rate/100
+        elite_size = int(args.population_size) * args.elitism_rate/100
         print "Using crossover size of "+str(crossover_size)+" and elitism size of "+str(elite_size)
         #generating the right lists, mols are picked for crossover like first with last second with second last etc
         crossover_mols,elite_mols = ga_setup_selection.tournament_selection(mol_list_en = ml_sorted_by_en, elitism_rate=elite_size,new_pop_size=crossover_size)
@@ -211,9 +225,13 @@ for ng in number_of_gens:
                 pairs_set.append((p[0],p[1]))
         #print len(pairs_set)
         children = []
-        for p in pairs_set:
-            kids = ga_crossover.crossover_pair_varying(p[0],p[1])
-            children+=kids
+        while True:
+            try:
+                kids = ga_ring_crossover.ring_crossover(p[0],p[1])
+                children+=kids
+                break
+            except (IndexError,RuntimeError,ValueError):
+                continue
         #print len(children)
         #combining elite and crossed over mols
         mol_list_2 = []
---
+++ ga_ring_crossover.py
@@ -0,0 +1,340 @@
+import random
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import random
+def find_dummy_atoms(mol):
+    '''finds the dummy atoms in a mol, returns atom ids'''
+    dummy_atoms = []
+    for atom in mol.GetAtoms():
+        at_num = atom.GetAtomicNum()
+        #print at_num
+        if at_num == 0:
+            index = atom.GetIdx()
+            neigh = []
+            neighbours = atom.GetNeighbors()
+            for n in neighbours:
+                neigh_id = n.GetIdx()
+                neigh.append(neigh_id)
+            dummy_atoms.append((index, neigh))
+            
+    return dummy_atoms
+
+def fragment_total(mol):
+    '''fragments molecule into all constituent rings, returns a list of each ring'''
+    frags = []
+    origmol = mol
+    for x in range(0,5):
+        if x == 4:
+            tempmol = origmol
+            cut_pts = return_cut_points(tempmol)
+            print cut_pts
+            cp = get_actual_cut_pts(tempmol,(x),cut_pts)
+            print cp
+            frag = frag_mol(tempmol,cp,bf='n')
+            frags.append(frag[1])
+            break
+        if x == 0:
+            tempmol = origmol
+        r = tempmol.GetRingInfo()
+        #print r.NumRings()
+        cut_pts = return_cut_points(tempmol)
+        print cut_pts
+        cp = get_actual_cut_pts(tempmol,1,cut_pts)
+        print cp
+        frag = frag_mol(tempmol,cp,bf='n')
+        frags.append(frag[0])
+        tempmol = frag[1]
+    print len(frags)
+    return frags
+
+def join_total(frags):
+    '''joins list of multiple fragments together, returns new mol'''
+    print len(frags)
+    join_pts = [0,1,2,3]
+    for x in join_pts:
+        #print x
+        if x == 0:
+            temp_mol = Chem.CombineMols(frags[x],frags[x+1])
+            atch_mol = join_mols(temp_mol)
+            #r = atch_mol.GetRingInfo()
+            #print r.NumRings()
+            continue
+        if x == 4:
+            break
+        else:
+            temp_mol = Chem.CombineMols(atch_mol, frags[x+1])
+            new_atch_mol = join_mols(temp_mol)
+            #r = new_atch_mol.GetRingInfo()
+            #print r.NumRings()
+            atch_mol = new_atch_mol
+    return atch_mol
+
+
+
+def ring_fixer(cross_mol,parent_mol):
+    '''two point crossover can lead to too short and too long molecules,
+    this function either adds or subtracts from the molecule'''
+    rings = cross_mol.GetRingInfo()
+    num_rings = rings.NumRings()
+    print "number of rings = "+str(num_rings)
+    if num_rings == 5:
+        new_mol = cross_mol
+    if num_rings < 5:
+        ring_ats = rings.AtomRings()[num_rings-1]
+        #print ring_ats
+        end_ats = []
+        for atom in cross_mol.GetAtoms():
+            if atom.GetIdx() in ring_ats:
+                if len(atom.GetBonds()) == 2:
+                     end_ats.append(atom.GetIdx())
+        #print end_ats
+        #print num_rings
+        cp = return_cut_points(parent_mol)
+        #print cp
+        ring_cp = [cp[num_rings-1] + cp[-num_rings]]
+        print ring_cp
+        #actual_cp =  (ring_cp[0]) + (ring_cp[1])
+        actual_cp = get_actual_cut_pts(parent_mol,num_rings,cp)
+        print actual_cp
+        #add_ring_cp = get_actual_cut_pts(parent_mol,num_rings-1,cp)
+        additional_ring = frag_mol(parent_mol,actual_cp,bf='n')[1]
+        #print Chem.MolToSmiles(cross_mol)
+        #print Chem.MolToSmiles(additional_ring)
+        te2_m = Chem.EditableMol(cross_mol)
+        te2_m.ReplaceAtom(end_ats[1], Chem.Atom(0))
+        te2_m.ReplaceAtom(end_ats[2], Chem.Atom(0))
+        tm2 = te2_m.GetMol()
+        Chem.SanitizeMol(tm2)
+        #print Chem.MolToSmiles(tm2)
+        temp_mol = Chem.CombineMols(tm2,additional_ring)
+        #new_mol = cross_mol
+        new_mol = join_mols(temp_mol)
+    if num_rings > 5:
+        #print num_rings
+        cut_pts = return_cut_points(cross_mol)
+        #print cut_pts
+        ring_ats = rings.AtomRings()[5]
+        #if n_diff >=2:
+        #    ring_ats = rings.AtomRings()[4]#num_rings-1]
+        #print ring_ats
+        cut_atoms = []
+        cut_atoms.append(ring_ats[-1])
+        cut_atoms.append(ring_ats[-2])
+        print cut_atoms
+        cp_tup = [(cut_at,cut_pt) for cut_at,cut_pt in cut_pts if cut_at in cut_atoms]
+        #actual_cp = get_actual_cut_pts(cross_mol,4,cut_pts)
+        #print actual_cp
+        frags = frag_mol(cross_mol,cp_tup,bf='n')
+        temp_mol = frags[0]
+        dum_ats = find_dummy_atoms(temp_mol)
+        #print dum_ats
+        #print dum_ats[0][1][0]
+        #print dum_ats[1][1][0]
+        te_m = Chem.EditableMol(temp_mol)
+        rem_at = te_m.RemoveAtom(dum_ats[1][0])
+        rem_at2 = te_m.RemoveAtom(dum_ats[0][0])
+        new_at = te_m.AddAtom(Chem.Atom(6))## these new atoms are added onto the end of the atom idx
+        new_at2 = te_m.AddAtom(Chem.Atom(6))##all orig atoms are moved down by 1
+        #print new_at
+        #print new_at2
+        te_m.AddBond(new_at,dum_ats[0][1][0],Chem.BondType.AROMATIC)
+        te_m.AddBond(new_at,new_at2,Chem.BondType.AROMATIC)
+        te_m.AddBond(new_at2,dum_ats[-1][-1][1],Chem.BondType.AROMATIC)
+        tm = te_m.GetMol()
+        Chem.SanitizeMol(tm)
+        #trings = tm.GetRingInfo()
+        #tnr = trings.NumRings()
+        new_mol = tm
+        #prop_cross.append(new_mol)
+    return new_mol
+
+
+
+def return_cut_points(mol):
+    '''returns cutting points for crossover, gets all 3 center C 
+    and returns specific ones for different rings'''
+    cut_pts = []
+    for atom in mol.GetAtoms(): ##can functionalise this snippet just to return cut_pts
+        index = atom.GetIdx()
+        at_num = atom.GetAtomicNum()
+        neigh = atom.GetNeighbors()
+        neighbours = []
+        if len(neigh) > 2:
+            for n in neigh:
+                neigh_id = n.GetIdx()
+                neighbours.append(neigh_id)
+            cut_pts.append((index, neighbours))
+    #actual_cp = cut_pts[num-1] + cut_pts[-num]
+    return cut_pts
+
+def frag_mol(mol,actual_cp,bf):
+    '''frag mol based on cutting points, add dummy atoms at points'''
+    frags = []
+    print actual_cp
+    atom1 = actual_cp[0][0]
+    atom2 = actual_cp[1][0]
+    atom_diff = abs(atom1 - atom2)
+    #print atom_diff
+    em = Chem.EditableMol(mol)
+    if atom_diff == 1:
+        em.RemoveBond(atom1,actual_cp[0][1][0])
+        em.RemoveBond(atom2,actual_cp[1][1][1])
+    else:
+        em.RemoveBond(atom1,actual_cp[0][1][1])
+        em.RemoveBond(atom2,actual_cp[1][1][0])
+    new_at = em.AddAtom(Chem.Atom(0))
+    if atom_diff == 1:
+        em.AddBond(actual_cp[0][1][0],new_at,Chem.BondType.AROMATIC)
+    else:
+        em.AddBond(actual_cp[0][1][1],new_at,Chem.BondType.AROMATIC)
+    new_at2 = em.AddAtom(Chem.Atom(0))
+    if atom_diff == 1:
+        em.AddBond(actual_cp[1][1][1],new_at2,Chem.BondType.AROMATIC)
+    else:
+        em.AddBond(actual_cp[1][1][0],new_at2,Chem.BondType.AROMATIC)
+    em.AddBond(new_at,new_at2,Chem.BondType.AROMATIC)
+    if bf == "y":
+        side = random.randint(1,2)
+        print side
+        if side == 1:
+            em.ReplaceAtom(atom1, Chem.Atom(0))
+            em.ReplaceAtom(actual_cp[0][1][0], Chem.Atom(0))
+        if side == 2:
+            em.ReplaceAtom(actual_cp[1][1][1], Chem.Atom(0))
+            em.ReplaceAtom(atom2, Chem.Atom(0))
+    if bf == 'n':
+        em.ReplaceAtom(atom1, Chem.Atom(0))
+        em.ReplaceAtom(atom2, Chem.Atom(0))
+    nm = em.GetMol()
+    frags = Chem.GetMolFrags(nm,asMols=True)
+    print len(frags)
+    return frags
+
+def join_mols(mol):
+    '''join fragged mols, based on dummy atom positions'''
+    dummy_atoms = find_dummy_atoms(mol)
+    #print dummy_atoms 
+    #print dummy_atoms[0][0]
+    temp_em = Chem.EditableMol(mol)
+    temp_em.RemoveAtom(dummy_atoms[-1][0])
+    temp_em.RemoveAtom(dummy_atoms[-2][0])
+    temp_em.ReplaceAtom(dummy_atoms[1][0],Chem.Atom(6))
+    temp_em.ReplaceAtom(dummy_atoms[0][0],Chem.Atom(6))
+    temp_em.AddBond(dummy_atoms[0][0],dummy_atoms[-2][1][0],Chem.BondType.AROMATIC)
+    temp_em.AddBond(dummy_atoms[1][0],dummy_atoms[-1][1][0],Chem.BondType.AROMATIC)
+    tm = temp_em.GetMol()
+    Chem.SanitizeMol(tm)
+    return tm
+
+
+def get_actual_cut_pts(mol,n,cut_pts):
+    rings = mol.GetRingInfo()
+    ring_ats = rings.AtomRings()[n]
+    actual_ca = ring_ats[-2:]
+    actual_cp = [(at,neigh) for at,neigh in cut_pts if at in actual_ca]
+    return actual_cp
+
+def single_point_crossover(m1,m2,bf):
+    '''single point crossover for rings'''
+    crossed_mols = []
+    n1 =  random.randint(1,4)
+    cp1 = return_cut_points(m1)
+    cp2 = return_cut_points(m2)
+    actual_cp1 = get_actual_cut_pts(m1,n1,cp1) 
+    actual_cp2 = get_actual_cut_pts(m2,n1,cp2)
+    frags1 = frag_mol(m1,actual_cp1,bf)
+    frags2 = frag_mol(m2,actual_cp2,bf)
+    temp_mol1 = Chem.CombineMols(frags1[0],frags2[1])
+    temp_mol2 = Chem.CombineMols(frags2[0],frags1[1])
+    crossed_mol1 = join_mols(temp_mol1)
+    crossed_mol2 = join_mols(temp_mol2)
+    crossed_mols.append(crossed_mol1)
+    crossed_mols.append(crossed_mol2)
+    return crossed_mols
+
+def double_point_crossover(m1,m2,bf):
+    '''double point crossover for rings'''
+    crossed_mols = []
+    n1 =  random.randint(1,4)
+    n2 =  random.randint(n1-1,4)
+    print n1
+    print n2
+    if n2 == 0:
+        n2 = 1
+    cp1 = return_cut_points(m1)
+    cp2 = return_cut_points(m2)
+    actual_cp1 = get_actual_cut_pts(m1,n1,cp1)
+    #print actual_cp1
+    actual_cp2 = get_actual_cut_pts(m2,n2,cp2)
+    #print actual_cp2
+    frags1 = frag_mol(m1,actual_cp1,bf)
+    frags2 = frag_mol(m2,actual_cp2,bf)
+    print frags1
+    print frags2
+    fm =  [Chem.MolToSmiles(x,True) for x in frags1]
+    fm2 = [Chem.MolToSmiles(x,True) for x in frags2]
+    print fm
+    print fm2
+    temp_mol1 = Chem.CombineMols(frags1[0],frags2[1])
+    temp_mol2 = Chem.CombineMols(frags2[0],frags1[1])
+    crossed_mol1 = join_mols(temp_mol1)
+    crossed_mol2 = join_mols(temp_mol2)
+    fixed_mol1 = ring_fixer(crossed_mol1,m1)
+    fixed_mol2 = ring_fixer(crossed_mol2,m2)
+    crossed_mols.append(fixed_mol1)
+    crossed_mols.append(fixed_mol2)
+    return crossed_mols
+
+def uniform_crossover(m1,m2):
+    '''uniform crossover for rings'''
+    crossed_mols = []
+    new_mol1 = []
+    new_mol2 = []
+    frags1 = fragment_total(m1)
+    frags2 = fragment_total(m2)
+    #print frags1
+    #print frags2
+    for x in range(0,5):
+        p = random.randint(0,100)
+        #print p
+        if p <= 50:
+            new_mol1.append(frags1[x])
+            new_mol2.append(frags2[x])
+        if p > 50:
+            new_mol1.append(frags2[x])
+            new_mol2.append(frags1[x])
+    crossed_mol1 = join_total(new_mol1)
+    crossed_mol2 = join_total(new_mol2)
+    crossed_mols.append(crossed_mol1)
+    crossed_mols.append(crossed_mol2)
+    return crossed_mols
+
+def ring_crossover(p1,p2):
+    '''crosses over 2 seperate molecules, using rings as the genome, also can create branched mols'''
+    p_smiles1 = Chem.MolToSmiles(p1)
+    p_smiles2 = Chem.MolToSmiles(p2)
+    m1 = Chem.MolFromSmiles(p_smiles1)
+    m2 = Chem.MolFromSmiles(p_smiles2)
+    print p_smiles1+" crossed with "+p_smiles2
+    child_mols = []
+    branch_factor = random.randint(0,100)
+    if branch_factor <= 5:
+        bf = 'y'
+    else:
+        bf = 'n'
+    if bf == 'y':
+        print "branching"
+    crossover_choice = random.randint(0,100)
+    if "(" in p_smiles1 or p_smiles2:
+        crossover_choice = random.randint(0,89)
+    print crossover_choice 
+    if crossover_choice <= 30:
+        print "crossover is single point"
+        child_mols = single_point_crossover(m1,m2,bf)
+    if crossover_choice > 30 and crossover_choice < 90:
+        print "crossover is double point"
+        child_mols = double_point_crossover(m1,m2,bf)
+    if crossover_choice >= 90:
+        print "crossover is uniform"
+        child_mols = uniform_crossover(m1,m2)
+    return child_mols
